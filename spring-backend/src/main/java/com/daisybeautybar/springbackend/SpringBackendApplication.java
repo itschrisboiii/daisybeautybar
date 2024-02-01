ea4271fa003dc667cae2cf7cef9c67a9ea43e075
itschrisboiii <cloya100@gmail.com>
2024-01-31 23:38:50 -0800

    added security to apis

--- spring-backend/src/main/java/com/daisybeautybar/springbackend/SpringBackendApplication.java
+++ spring-backend/src/main/java/com/daisybeautybar/springbackend/SpringBackendApplication.java
@@ -1,15 +1,17 @@
 package com.daisybeautybar.springbackend;
 
+import com.daisybeautybar.springbackend.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class SpringBackendApplication {
 
 	public static void main(String[] args) {
 
-		ApplicationContext apc = SpringApplication.run(SpringBackendApplication.class, args);
+		SpringApplication.run(SpringBackendApplication.class, args);
 //		for (String s : apc.getBeanDefinitionNames()) {
 //			System.out.println(s);
 //		}
